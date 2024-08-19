f074766339b52fd8ae0e5827e1e5de03c873b071
joshuamadu4 <joshuamadu4@gmail.com>
2024-08-19 10:43:14 +0100

    Corrected Routes

--- app/layout.js
+++ app/layout.js
@@ -4,8 +4,8 @@ import './globals.css'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-  title: 'Lama Dev Next.js Admin Dashboard',
-  description: 'Next.js Tutorial',
+  title: 'TBIWP Admin Dashboard',
+  description: 'Tukur Buratai Institute of War and Peace',
 }
 
 export default function RootLayout({ children }) {
